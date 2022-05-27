d0ebb507b31c965160a51a1caf30ada980233130
Sohaila Diab <70928356+SohailaDiab@users.noreply.github.com>
2022-05-27 05:26:42 +0200

    Create main.cpp

---
+++ Matrix Calculator/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+#include "matrixmenu.h"
+#include "matrix.h"
+using namespace std;
+int main()
+{
+    MatrixCalculator <double>obj;
+    Matrix <double>ob2;
+    obj.menu();
+    return 0;
+}
